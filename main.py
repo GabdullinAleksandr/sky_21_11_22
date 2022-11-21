99c75fa02e77a903056b395fc64495153ca2f4f4
Gabdullin_Alex <alexrexp@gmail.com>
2022-11-21 18:24:34 +0300

    task 1.3

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ with open('devices.json', 'r') as f:
 def main():
     while True:
         us_input = input('Введите имя устройства - ')
-        us_input_par = input('Введите имя параметра - ')
         try:
-            print(content[us_input][us_input_par])
+            par = tuple(i for i in content[us_input].keys())
         except KeyError:
             print('incorrect input')
             continue
+        us_input_par = input(f'Введите имя параметра - {par} - ').lower()
+        print(content[us_input][us_input_par]) if us_input_par in par \
+            else print('Такого параметра нет')
 
 
 if __name__ == '__main__':
     main()
-
